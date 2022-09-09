6063e21965cc28e7abfc45e85db2fbe1634374e2
sboussakla <soufianeboussakla@gmail.com>
2022-09-09 11:53:25 +0200

    Update AccountService.java

--- src/main/java/com/sg/kata/services/account/AccountService.java
+++ src/main/java/com/sg/kata/services/account/AccountService.java
@@ -5,6 +5,7 @@ import com.sg.kata.exceptions.AccountNotFoundException;
 import org.springframework.transaction.annotation.Transactional;
 
 public interface AccountService {
+    @Transactional(readOnly = true)
     Account getAccountById(Long accountId) throws AccountNotFoundException;
 
     @Transactional
